bcd798d2602c68651fb0c11559e77cb57e7c324c
Oliver Hookins <ohookins@gmail.com>
2026-08-16 15:36:28 +1000

    Make tags clickable with dedicated tag pages
    - Add tag page template that lists all posts for a given tag
    - Generate /tags/{slug} pages for each unique tag at build time
    - Update TagCluster to render tags as Gatsby Link components
    
    Closes #79

--- src/components/TagCluster/index.js
+++ src/components/TagCluster/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "gatsby";
+
+const slugify = (text) =>
+  text.toLowerCase().replace(/[^a-z0-9]+/g, "-").replace(/(^-|-$)/g, "");
 
 const TagCluster = ({ tags }) => {
   const finalTags = tags || [];
@@ -8,9 +12,13 @@ const TagCluster = ({ tags }) => {
     <div className="sans-serif f7">
       <span className="b">Tagged with: </span>
       {finalTags.map(tag => (
-        <span className="f7 br2 ph2 ph2-m ph3-ns pv0 pv0-m pv1-ns ma1 dib white bg-blue">
+        <Link
+          key={tag.realname}
+          to={`/tags/${slugify(tag.realname)}`}
+          className="f7 br2 ph2 ph2-m ph3-ns pv0 pv0-m pv1-ns ma1 dib white bg-blue no-underline"
+        >
           {tag.realname}
-        </span>
+        </Link>
       ))}
     </div>
   );
---
+++ src/templates/tag.js
@@ -0,0 +1,85 @@
+import React from "react";
+import { Link, graphql } from "gatsby";
+import Layout from "../components/Layout";
+import PublishLine from "../components/PublishLine";
+
+const BlogPost = ({ node }) => {
+  return (
+    <li>
+      <Link
+        to={"/posts/" + node.slug}
+        className="indexLink b cabin f4 f4-m f3-ns"
+      >
+        {node.title}
+      </Link>
+      <PublishLine
+        author={node.author.firstName}
+        published={node.published}
+        category={node.category.realname}
+      />
+      <div
+        dangerouslySetInnerHTML={{
+          __html: node.body.childMarkdownRemark.excerpt
+        }}
+        className="indexSummary"
+      />
+    </li>
+  );
+};
+
+const TagPage = ({ pageContext, data }) => {
+  const { tagName } = pageContext;
+  const posts = data.allContentfulPost.edges;
+
+  return (
+    <Layout>
+      <div>
+        <h2 className="cabin f4 f3-ns">
+          Posts tagged with{" "}
+          <span className="br2 ph2 pv1 white bg-blue">{tagName}</span>
+        </h2>
+        <ul className="list pl0">
+          {posts.map(edge => (
+            <BlogPost key={edge.node.slug} node={edge.node} />
+          ))}
+        </ul>
+        <div className="cabin tc ma4">
+          <Link to="/" className="b">← Back to all posts</Link>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default TagPage;
+
+export const pageQuery = graphql`
+  query tagPageQuery($tagName: String!) {
+    allContentfulPost(
+      filter: {
+        node_locale: { eq: "en-US" }
+        tags: { elemMatch: { realname: { eq: $tagName } } }
+      }
+      sort: { fields: [published], order: DESC }
+    ) {
+      edges {
+        node {
+          title
+          slug
+          author {
+            firstName
+          }
+          category {
+            realname
+          }
+          published
+          body {
+            childMarkdownRemark {
+              excerpt(pruneLength: 400)
+            }
+          }
+        }
+      }
+    }
+  }
+`;
